7f93f8dc39171d927a31ca4905ae2caef35e907b
Naelton Santos <naeon02@gmail.com>
2019-06-22 15:02:09 -0300

    Calculadora usando while.

---
+++ calculadora_while.php
@@ -0,0 +1,14 @@
+<?php
+
+    $numero = 8;
+
+    $contador = 1;
+
+    while ($contador <= 10) {
+
+        echo $contador . " x " . $numero . " = " . ($contador*$numero) . "<br>";
+        
+        $contador++;
+    }
+
+?>
