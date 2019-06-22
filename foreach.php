eeb3041fbff18709b818d9a7578cb7724f1f58c6
Naelton Santos <naeon02@gmail.com>
2019-06-22 15:26:31 -0300

    Exemplo usando  foreach

---
+++ foreach.php
@@ -0,0 +1,20 @@
+<?php
+
+    $planetas = [
+        'Mercúrio',
+        'Vênus',
+        'Terra',
+        'Marte',
+        'Júpiter',
+        'Saturno',
+        'Urano',
+        'Neturno',
+    ];
+
+    foreach ($planetas as $planeta) {
+
+        echo $planeta . "<br>";
+        
+    }
+
+?>
