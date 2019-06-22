64428c2d008b4a07945ac7af58d71a50d5db2c2f
Naelton Santos <naeon02@gmail.com>
2019-06-22 09:10:57 -0300

    Operadores de comparação

---
+++ operadores_comparacao.php
@@ -0,0 +1,19 @@
+<?php
+
+    $a = 10;
+    $b = 10;
+
+//    if ($a == $b) {
+//        echo "A é igual a B";
+//    } else {
+//        echo " A é diferente de B";
+//    }
+
+    if ($a >= $b) {
+        echo " A é maior ou igual a B";
+    } else {
+        echo " A é menor que B";
+    }
+    
+   
+?>
