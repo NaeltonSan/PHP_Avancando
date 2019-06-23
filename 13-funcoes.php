481b80ab73b3fe33168f2f01b2d14dde3d9ff112
Naelton Santos <naeon02@gmail.com>
2019-06-23 14:11:08 -0300

    Funções

---
+++ 13-funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+    function escreva_nome($nome_aluno){
+        echo "Olá $nome_aluno!<br>";
+    }
+
+   echo escreva_nome('Naelton');
+
+   echo escreva_nome('João');
+
+   echo escreva_nome('Maria');
+
+?>
