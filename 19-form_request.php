bc9e8e407c3f20383848ec6434304bc969fd7403
Naelton Santos <naeon02@gmail.com>
2019-06-23 15:41:59 -0300

    formulario simples, para o funcionamento do arquivo 18-request

---
+++ 19-form_request.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="18-request.php?nome=naelton" method="post">
+            <label for="">Cor : </label>
+            <input type="text" name="cor" id="">
+            <button type="submit">Enviar</button>
+        </form>
+    </body>
+</html>
